e02a5b4ef69f590009aa57640ac9b43eaa8ad97e
Rafal Witczak <raf_w87@wp.pl>
2020-08-30 16:36:11 +0200

    feat(arduino): add sensor + battery status

--- arduino/altimeter_ble/altimeter_ble.ino
+++ arduino/altimeter_ble/altimeter_ble.ino
@@ -1,9 +1,78 @@
-void setup() {
-  // put your setup code here, to run once:
+#define BATTERY_LEVEL A7
+#define BATTERY_EMPTY_READING 310
+#define BATTERY_FULL_READING 615
 
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme;
+
+float seaLevelPressure = 1013.25;
+unsigned bme280Status;
+float initialAltitude = 0;
+float initialBatteryLevel = 0;
+
+void setup() {
+  pinMode(BATTERY_LEVEL, INPUT);
+  
+  Serial.begin(115200);
+
+  initBME280();
+  initialAltitude = getAltitude();
+  initialBatteryLevel = getBatteryLevel();
+  Serial.println("Altitude Battery");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (bme280Status) {
+    float altitude = bme.readAltitude(seaLevelPressure);
+    if(isnan(altitude)) {
+      Serial.println("BME280 is disconnected.");
+      initBME280();
+      return;
+    }
+    float batteryLevel = getBatteryLevel();
 
+    Serial.print(altitude);
+    Serial.print(" ");
+    Serial.print(batteryLevel);
+    Serial.println();
+    
+  } else {
+    Serial.println("BME280 is disconnected.");
+  }
+  delay(500);
+}
+
+float getAltitude() {
+  bool isDisconnected;
+  return getAltitude(&isDisconnected);
+}
+
+float getAltitude(bool *isConnected) {
+  float altitude = bme.readAltitude(seaLevelPressure);
+  if(isnan(altitude)) {
+    *isConnected = false;
+    return 0;
+  } else {
+    *isConnected = true;
+    return altitude;
+  }
+}
+
+float getBatteryLevel() {
+  float batteryRaw = analogRead(BATTERY_LEVEL);
+  return constrain(map(batteryRaw, BATTERY_EMPTY_READING, BATTERY_FULL_READING, 0, 100), 0, 100);
+}
+
+void initBME280() {
+  bme280Status = bme.begin(0x76);
+  Serial.println("Test 2");
+  if (!bme280Status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+  }
 }
